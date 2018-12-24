7b664a31c22d9609d8643e9421f27bc56dc6a013
Formolin <liujiang.hrb@163.com>
2018-12-24 13:47:56 +0800

    轮播图样式搞定

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>
-        <h1>home</h1>
-    </div>
+  <div>
+    <mt-swipe :auto="2000">
+      <mt-swipe-item>1</mt-swipe-item>
+      <mt-swipe-item>2</mt-swipe-item>
+      <mt-swipe-item>3</mt-swipe-item>
+    </mt-swipe>
+  </div>
 </template>
 
 <script>
-    
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.mint-swipe {
+  height: 200px;
+  margin-top: -20px;
+  .mint-swipe-item {
+    &:nth-child(1) {
+      background-color: yellowgreen;
+    }
+    &:nth-child(2) {
+      background-color: burlywood;
+    }
+    &:nth-child(3) {
+      background-color: grey;
+    }
+  }
+}
 </style>
